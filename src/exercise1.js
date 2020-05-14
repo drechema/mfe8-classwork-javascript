4e3ff93fe46bed57ac7508de9969f2357c088859
David Reche <david.reche@intersystems.com>
2020-05-14 18:56:19 +0200

    Exercise1

---
+++ src/exercise1.js
@@ -0,0 +1,7 @@
+
+function exercise1() {
+    console.log("Exercise 1:");
+    var testArray = ["one","two","three","four"];
+    const head = ([first]) => first;
+    console.log("using head: ",head(testArray));
+}

3ef980c061f848dc694fde326344b66c157640a7
David Reche Martinez <dreche@ES-MBP2019RECHE.local>
2020-05-19 12:02:01 +0200

    Exercise 4

--- src/exercise4.ts
+++ src/exercise4.ts
@@ -1,3 +1,8 @@
+interface Book {
+    title:string;
+    isRead: boolean;
+}
+
 /**
  * Decide if a book is read or not
  * @param books array of books
@@ -5,18 +10,19 @@
  * @returns {boolean} true/false
  */
 function isBookRead(books, titleToSearch) {
-    var b = books.find(function (b) { return b.title == titleToSearch; });
+    const b = books.find(b=>b.title==titleToSearch);
     return b ? b.isRead : false;
 }
+
 function exercise4() {
     console.log("Testing Exercise 4:");
-    var books = [
+    const books = [
         { title: "Harry Potter y la piedra filosofal", isRead: true },
         { title: "Canción de hielo y fuego", isRead: false },
         { title: "Devastación", isRead: true },
     ];
     console.log("books:", books);
-    console.log("isBookRead(books, 'Devastación')", isBookRead(books, "Devastación")); // true
-    console.log("isBookRead(books, 'Canción de hielo y fuego')", isBookRead(books, "Canción de hielo y fuego")); // false
-    console.log("isBookRead(books, 'Los Pilares de la Tierra')", isBookRead(books, "Los Pilares de la Tierra")); // false
+    console.log("isBookRead(books, 'Devastación')",isBookRead(books, "Devastación")); // true
+    console.log("isBookRead(books, 'Canción de hielo y fuego')",isBookRead(books, "Canción de hielo y fuego")); // false
+    console.log("isBookRead(books, 'Los Pilares de la Tierra')",isBookRead(books, "Los Pilares de la Tierra")); // false
 }
